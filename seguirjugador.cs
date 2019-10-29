4f71c7d2ee43d9bae57b52870dcc0522802f8fe7
fojomars <junior.mars125@gmail.com>
2019-10-29 17:25:17 +0100

    Script CameraController

---
+++ seguirjugador.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seguirjugador : MonoBehaviour
+{
+    public Transform jugador;
+    UnityEngine.AI.NavMeshAgent enemigo;
+    private bool dentro = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemigo = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!dentro)
+        {
+            enemigo.destination = jugador.position;
+        }
+        if (dentro)
+        {
+            enemigo.destination = this.transform.position;
+        }
+    }
+}
+
